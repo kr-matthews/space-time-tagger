46ea86ecdb07d5dc56a56a3eadcf106fddfd243a
Kevin Matthews <kvnrmatthews@gmail.com>
2024-09-14 16:41:51 -0700

    Bump minSdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.space_timetagger"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

253bbed1d2df5beb5a99b2bbd677d6c571191605
Kevin Matthews <kvnrmatthews@gmail.com>
2024-09-14 22:05:27 -0700

    Remove enableEdgeToEdge

--- app/src/main/java/com/example/space_timetagger/MainActivity.kt
+++ app/src/main/java/com/example/space_timetagger/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.space_timetagger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,7 +13,6 @@ import com.example.space_timetagger.ui.theme.SpaceTimeTaggerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SpaceTimeTaggerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->

fd9d42728b067e3154259e79adbd80f7a250e57e
Kevin Matthews <kvnrmatthews@gmail.com>
2024-09-24 18:54:10 -0700

    Remove unnecessary selectedSessionId from SessionsViewModel

--- app/src/main/java/com/example/space_timetagger/ui/sessions/SessionsViewModel.kt
+++ app/src/main/java/com/example/space_timetagger/ui/sessions/SessionsViewModel.kt
@@ -13,9 +13,6 @@ class SessionsViewModel(
     private val _sessions = MutableStateFlow(initialList)
     val sessions = _sessions.asStateFlow()
 
-    private val _selectedSessionId = MutableStateFlow<String?>(null)
-    val selectedSessionId = _selectedSessionId.asStateFlow()
-
     val callbacks = object : SessionsCallbacks {
         override fun new(name: String?) {
             _sessions.update {
@@ -24,16 +21,12 @@ class SessionsViewModel(
         }
 
         override fun delete(id: String) {
-            if (selectedSessionId.value == id) {
-                _selectedSessionId.update { null }
-            }
             _sessions.update {
                 it.toMutableList().apply { removeIf { session -> session.id == id } }
             }
         }
 
         override fun clearAll() {
-            _selectedSessionId.update { null }
             _sessions.update { listOf() }
         }
     }

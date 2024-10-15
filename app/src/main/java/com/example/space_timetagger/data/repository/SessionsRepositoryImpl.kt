680f6d81e4825ebf3f96420c1744e11f4ac5a4c9
Kevin Matthews <kvnrmatthews@gmail.com>
2024-10-14 21:58:03 -0700

    Update sessions repository to return copies instead of modifying in-place

--- app/src/main/java/com/example/space_timetagger/data/repository/SessionsRepositoryImpl.kt
+++ app/src/main/java/com/example/space_timetagger/data/repository/SessionsRepositoryImpl.kt
@@ -5,76 +5,79 @@ import com.example.space_timetagger.domain.model.TagModel
 import com.example.space_timetagger.domain.repository.SessionsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 
 class SessionsRepositoryImpl : SessionsRepository {
-    private val _sessions = MutableStateFlow(mutableListOf<SessionModel>())
+    private val _sessions = MutableStateFlow(listOf<SessionModel>())
 
-    override fun sessions() = _sessions
+    override fun sessions() = _sessions.asStateFlow()
 
     override fun session(id: String): Flow<SessionModel?> =
         _sessions.map { list -> list.find { session -> session.id == id } }
 
     override suspend fun newSession(name: String?): String {
         val newSession = SessionModel(name)
-        _sessions.update { it.apply { add(newSession) } }
+        _sessions.update { it.toMutableList().apply { add(newSession) } }
         return newSession.id
     }
 
     override suspend fun renameSession(id: String, newName: String?) {
         _sessions.update { list ->
-            list.apply {
+            list.toMutableList().apply {
                 val index = list.indexOfFirst { it.id == id }
                 if (index > -1) {
-                    list[index] = get(index).copy(name = newName)
+                    set(index, get(index).copy(name = newName))
                 }
-            }
+            }.toList()
         }
     }
 
     override suspend fun addTagToSession(id: String, tag: TagModel) {
         _sessions.update { list ->
-            list.apply {
+            list.toMutableList().apply {
                 val index = list.indexOfFirst { it.id == id }
                 if (index > -1) {
                     val session = get(index)
                     val newTags = session.tags.toMutableList().apply { add(tag) }
-                    list[index] = session.copy(tags = newTags)
+                    set(index, session.copy(tags = newTags))
                 }
-            }
+            }.toList()
         }
     }
 
     override suspend fun removeTagFromSession(id: String, tag: TagModel) {
         _sessions.update { list ->
-            list.apply {
+            list.toMutableList().apply {
                 val index = list.indexOfFirst { it.id == id }
                 if (index > -1) {
                     val session = get(index)
                     val newTags = session.tags.toMutableList().apply { remove(tag) }
-                    list[index] = session.copy(tags = newTags)
+                    set(index, session.copy(tags = newTags))
                 }
-            }
+            }.toList()
         }
     }
 
     override suspend fun removeAllTagsFromSession(id: String) {
         _sessions.update { list ->
-            list.apply {
+            list.toMutableList().apply {
                 val index = list.indexOfFirst { it.id == id }
                 if (index > -1) {
-                    list[index] = get(index).copy(tags = listOf())
+                    set(index, get(index).copy(tags = listOf()))
                 }
-            }
+            }.toList()
         }
     }
 
     override suspend fun deleteSession(id: String) {
-        _sessions.update { list -> list.apply { removeIf { it.id == id } } }
+        _sessions.update { list ->
+            list.toMutableList().apply { removeIf { it.id == id } }.toList()
+        }
     }
 
     override suspend fun deleteAllSessions() {
-        _sessions.update { mutableListOf() }
+        _sessions.update { listOf() }
     }
 }

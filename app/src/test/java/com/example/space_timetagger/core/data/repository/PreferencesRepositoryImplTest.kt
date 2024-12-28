8fcede1ad47037b5e57912a4eda94e25290bf11d
Kevin Matthews <kvnrmatthews@gmail.com>
2024-12-27 21:53:36 -0800

    Create PreferencesRepositoryImplTest

---
+++ app/src/test/java/com/example/space_timetagger/core/data/repository/PreferencesRepositoryImplTest.kt
@@ -0,0 +1,92 @@
+package com.example.space_timetagger.core.data.repository
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.preferencesOf
+import assertk.assertThat
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
+import com.example.space_timetagger.CoroutineTestRule
+import com.example.space_timetagger.core.domain.repository.PreferencesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class PreferencesRepositoryImplTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    var coroutineTestRule = CoroutineTestRule()
+
+    @Mock
+    private lateinit var preferencesDataStore: DataStore<Preferences>
+
+    private lateinit var preferencesRepository: PreferencesRepository
+
+    private fun initializeRepository(mockPreferences: Preferences?) {
+        mockPreferences?.let { whenever(preferencesDataStore.data).thenReturn(flowOf(it)) }
+        preferencesRepository = PreferencesRepositoryImpl(
+            preferencesDataStore = preferencesDataStore,
+        )
+    }
+
+    @Test
+    fun initiallyWithEmptyPreferences_taggingLocationIsEnabledProducesFalse() = runTest {
+        initializeRepository(mockEmptyPreferences)
+        assertThat(preferencesRepository.taggingLocationIsEnabled.first()).isFalse()
+    }
+
+    @Test
+    fun initiallyWithEnabledLocationPreferences_taggingLocationIsEnabledProducesTrue() = runTest {
+        initializeRepository(mockEnabledLocationPreferences)
+        assertThat(preferencesRepository.taggingLocationIsEnabled.first()).isTrue()
+    }
+
+    @Test
+    fun initiallyWithDisabledLocationPreferences_taggingLocationIsEnabledProducesFalse() = runTest {
+        initializeRepository(mockDisabledLocationPreferences)
+        assertThat(preferencesRepository.taggingLocationIsEnabled.first()).isFalse()
+    }
+
+    @Ignore("Says checked exception is invalid for this method, not sure why")
+    @Test
+    fun whenDataStoreThrowsIOException_taggingLocationIsEnabledProducesFalse() = runTest {
+        whenever(preferencesDataStore.data).thenThrow(IOException())
+        initializeRepository(null)
+        assertThat(preferencesRepository.taggingLocationIsEnabled.first()).isFalse()
+    }
+
+    // unsure how best to write test
+    @Test
+    fun enableTaggingLocation_callsDataStoreEdit() = runTest {
+        initializeRepository(mockDisabledLocationPreferences)
+        preferencesRepository.enableTaggingLocation()
+        verify(preferencesDataStore).edit(any())
+    }
+
+    // unsure how best to write test
+    @Test
+    fun disableTaggingLocation_callsDataStoreEdit() = runTest {
+        initializeRepository(mockEnabledLocationPreferences)
+        preferencesRepository.disableTaggingLocation()
+        verify(preferencesDataStore).edit(any())
+    }
+
+    private val mockEmptyPreferences = emptyPreferences()
+    private val mockEnabledLocationPreferences = preferencesOf(TAGGING_LOCATION to true)
+    private val mockDisabledLocationPreferences = preferencesOf(TAGGING_LOCATION to false)
+}

e70e38e829954753e54ab0f8ac2226843d6a7466
Kevin Matthews <kvnrmatthews@gmail.com>
2025-01-23 22:09:55 -0800

    Use helper functions in MainActivity

--- app/src/main/java/com/example/space_timetagger/MainActivity.kt
+++ app/src/main/java/com/example/space_timetagger/MainActivity.kt
@@ -19,19 +19,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            setShowWhenLocked(true)
-        }
+        setUpShowOnLockScreen()
 
-        lifecycleScope.launch {
-            App.appModule.preferencesRepository.keepScreenOnIsEnabled.collect { isEnabled ->
-                if (isEnabled) {
-                    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                } else {
-                    window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                }
-            }
-        }
+        setUpMonitorKeepingScreenOn()
 
         setContent {
             val navController = rememberNavController()
@@ -39,6 +29,24 @@ class MainActivity : ComponentActivity() {
             SpaceTimeTaggerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     SttNavHost(navController, Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+    private fun setUpShowOnLockScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+        }
+    }
+
+    private fun setUpMonitorKeepingScreenOn() {
+        lifecycleScope.launch {
+            App.appModule.preferencesRepository.keepScreenOnIsEnabled.collect { isEnabled ->
+                if (isEnabled) {
+                    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+                } else {
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
                 }
             }
         }

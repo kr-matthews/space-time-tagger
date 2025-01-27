7f72065905d08cb291d2f261bcb099fdc41961ee
Kevin Matthews <kvnrmatthews@gmail.com>
2025-01-26 19:08:10 -0800

    Use clickable modifier's enabled, instead of a conditional

--- app/src/main/java/com/example/space_timetagger/sessions/presentation/sessionDetail/SessionDetail.kt
+++ app/src/main/java/com/example/space_timetagger/sessions/presentation/sessionDetail/SessionDetail.kt
@@ -47,7 +47,6 @@ import androidx.compose.ui.unit.sp
 import com.example.space_timetagger.R
 import com.example.space_timetagger.core.presentation.ConfirmationDialog
 import com.example.space_timetagger.core.presentation.formatShortDateLongTime
-import com.example.space_timetagger.core.presentation.thenIf
 import com.example.space_timetagger.sessions.presentation.models.SessionDetailUiModel
 import com.example.space_timetagger.sessions.presentation.models.TagUiModel
 import com.example.space_timetagger.ui.theme.SpaceTimeTaggerTheme
@@ -66,13 +65,9 @@ fun SessionDetail(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
             .fillMaxWidth()
-            .thenIf(
-                condition = session.tapAnywhereIsEnabled,
-                ifTrue = {
-                    clickable(
-                        onClick = { onEvent(SessionDetailEvent.TapAnywhere(OffsetDateTime.now())) },
-                    )
-                },
+            .clickable(
+                onClick = { onEvent(SessionDetailEvent.TapAnywhere(OffsetDateTime.now())) },
+                enabled = session.tapAnywhereIsEnabled
             )
     ) {
         Header(

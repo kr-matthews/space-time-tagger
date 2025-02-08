2289c373b4897851e122673e1e57c00ff0bf9c91
Kevin Matthews <kvnrmatthews@gmail.com>
2025-02-07 22:16:06 -0800

    Restrict top bar title to 2 lines

--- app/src/main/java/com/example/space_timetagger/core/presentation/MyTopBar.kt
+++ app/src/main/java/com/example/space_timetagger/core/presentation/MyTopBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -27,7 +28,7 @@ fun MyTopBar(
     actions: @Composable (RowScope.() -> Unit) = {}
 ) {
     TopAppBar(
-        title = { Text(text = title) },
+        title = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) },
         navigationIcon = {
             onBackTap?.let {
                 IconButton(onClick = it) {

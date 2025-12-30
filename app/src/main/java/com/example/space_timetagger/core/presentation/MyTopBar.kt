188c2c9311433d32f124c9afe26bb5bc0882ec06
Kevin Matthews <kvnrmatthews@gmail.com>
2025-12-29 22:30:09 -0800

    Move to material3 back arrow

--- app/src/main/java/com/example/space_timetagger/core/presentation/MyTopBar.kt
+++ app/src/main/java/com/example/space_timetagger/core/presentation/MyTopBar.kt
@@ -1,8 +1,6 @@
 package com.example.space_timetagger.core.presentation
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -30,7 +28,7 @@ fun MyTopBar(
             onBackTap?.let {
                 IconButton(onClick = it) {
                     Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        painter = painterResource(R.drawable.ic_arrow_back),
                         contentDescription = stringResource(R.string.back)
                     )
                 }
